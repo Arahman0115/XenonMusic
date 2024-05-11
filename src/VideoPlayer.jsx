f1ccd5363ccaceb67e9a34d4c23553a72532a917
Arahman0115 <ahnaf.rahman306@gmail.com>
2024-05-10 20:11:25 -0400

    muted vid

--- src/VideoPlayer.jsx
+++ src/VideoPlayer.jsx
@@ -1,7 +1,7 @@
 //VideoPlayer.jsx
 function VideoPlayer({ videoId, className }) {
     if (!videoId) return null;
-    const embedUrl = `https://www.youtube.com/embed/${videoId}?autoplay=1`
+    const embedUrl = `https://www.youtube.com/embed/${videoId}?autoplay=1mute=1`
   
     return (
       <div className="flex justify-center mt-10">
